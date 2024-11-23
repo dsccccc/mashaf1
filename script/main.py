c54086e160f7c93e6c80956a2842ce85e400b240
dsccccc <deshuchen97+dsc@gmail.com>
2024-11-23 17:25:24 +0800

    Create main.py

---
+++ script/main.py
@@ -0,0 +1,21 @@
+import requests
+from json import loads
+from bs4 import BeautifulSoup
+
+
+def reader(url: str) -> BeautifulSoup:
+    response = requests.get(url)
+    assert response.status_code == 200
+    return BeautifulSoup(response.text, 'html.parser')
+
+
+def parser(soup: BeautifulSoup) -> str:
+    try:
+        script = loads(soup.find('script', {'id': '__NEXT_DATA__'}).get_text())
+        return script['props']['pageProps']['liveDetailProp']['playAddr']['ori_m3u8']
+    except TypeError:
+        return 'N/A'
+
+
+if __name__ == '__main__':
+    print(parser(reader('https://haixingtiyu.live/live/1069')))
