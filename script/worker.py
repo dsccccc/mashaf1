e209fac791913c9297d63701f81442992b96cf37
dsccccc <deshuchen97+dsc@gmail.com>
2024-11-23 17:43:57 +0800

    Create worker.py

---
+++ script/worker.py
@@ -0,0 +1,10 @@
+def replacer(table: str, input_file: str = './tmp/README.md', output_file: str = 'README.md', reg: str = '<!-- URL -->'):
+    with open(input_file, 'r') as f:
+        data = f.read()
+    data = data.replace(reg, table)
+    with open(output_file, 'w') as f:
+        f.write(data)
+
+if __name__ == '__main__':
+    from script.main import *
+    replacer(parser(reader('https://haixingtiyu.live/live/1069')))
